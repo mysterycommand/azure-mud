83cf87ab82a598fb61b242f4e75b8764720032b3
Matt Hayes <matt@mysterycommand.com>
2022-09-04 13:54:15 -0500

    refactor: make message-item a 'dumb' wrapper

--- src/components/messages/MessageItem/MessageItem.tsx
+++ src/components/messages/MessageItem/MessageItem.tsx
@@ -1,29 +1,7 @@
 import React, { FC } from 'react'
-import { Message } from '../../../message'
-import MessageView from '../../MessageView'
-import { messageId } from '../utils'
 
 import './MessageItem.css'
 
-interface MessageItemProps {
-  message: Message;
-  hideTimestamp: boolean;
-  msgIndex: number;
-}
-
-export const MessageItem: FC<MessageItemProps> = ({
-  message,
-  hideTimestamp,
-  msgIndex
-}) => {
-  return (
-    <li className="message-item">
-      <MessageView
-        message={message}
-        id={messageId(message)}
-        hideTimestamp={hideTimestamp}
-        msgIndex={msgIndex}
-      />
-    </li>
-  )
-}
+export const MessageItem: FC = ({ children }) => (
+  <li className="message-item">{children}</li>
+)
