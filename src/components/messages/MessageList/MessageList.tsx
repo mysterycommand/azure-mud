329c5fdc270949ec32b8fb6f866b03881b315bdf
Matt Hayes <matt@mysterycommand.com>
2022-09-04 13:54:35 -0500

    refactor: reimplement hideTimestamp function

--- src/components/messages/MessageList/MessageList.tsx
+++ src/components/messages/MessageList/MessageList.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { Message } from '../../../message'
+import MessageView from '../../MessageView'
 import { MessageItem } from '../MessageItem'
 import { messageId } from '../utils'
 
@@ -9,17 +10,36 @@ interface MessageListProps {
   messages: Message[];
 }
 
+const THREE_MINUTES = 1_000 * 60 * 3
+const shouldHideTimestamp = (
+  message: Message,
+  previousMessage: Message | undefined
+): boolean =>
+  previousMessage &&
+  'userId' in previousMessage &&
+  'userId' in message &&
+  previousMessage.userId === message.userId &&
+  new Date(message.timestamp).getTime() -
+    new Date(previousMessage.timestamp).getTime() <
+    THREE_MINUTES
+
 export const MessageList: FC<MessageListProps> = ({ messages }) => {
   return (
     <ol className="message-list">
-      {messages.map((message, i) => (
-        <MessageItem
-          key={messageId(message)}
-          message={message}
-          hideTimestamp={false}
-          msgIndex={i}
-        />
-      ))}
+      {messages.map((message, i) => {
+        const hideTimestamp = shouldHideTimestamp(message, messages[i - 1])
+
+        return (
+          <MessageItem key={messageId(message)}>
+            <MessageView
+              message={message}
+              id={messageId(message)}
+              hideTimestamp={hideTimestamp}
+              msgIndex={i}
+            />
+          </MessageItem>
+        )
+      })}
     </ol>
   )
 }
