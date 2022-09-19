f4cb1aca4cfd96a11713b9d6ed263d5db1b092b1
Matt Hayes <matt@mysterycommand.com>
2022-09-18 22:51:34 -0500

    feat: bring in message list from other branch

---
+++ src/components/MessageList/MessageList.tsx
@@ -0,0 +1,131 @@
+import React, {
+  FC,
+  MutableRefObject,
+  UIEventHandler,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef
+} from 'react'
+
+import {
+  ActivateAutoscrollAction,
+  DeactivateAutoscrollAction
+} from '../../Actions'
+import { DispatchContext } from '../../App'
+
+import { Message, MessageType } from '../../message'
+import MessageView from '../MessageView'
+
+import './MessageList.css'
+
+interface MessageListProps {
+  messages: Message[];
+  autoscroll: boolean;
+}
+
+const THREE_MINUTES = 1_000 * 60 * 3
+const shouldHideTimestamp = (
+  message: Message,
+  previousMessage: Message | undefined
+): boolean =>
+  previousMessage &&
+  'userId' in previousMessage &&
+  'userId' in message &&
+  previousMessage.userId === message.userId &&
+  new Date(message.timestamp).getTime() -
+    new Date(previousMessage.timestamp).getTime() <
+    THREE_MINUTES
+
+const useAutoscrollTo = (
+  selector: string,
+  autoscroll: boolean
+): MutableRefObject<HTMLOListElement> => {
+  const scrollContainerRef = useRef<HTMLOListElement>()
+
+  const mutationCallback = useCallback(
+    (mutations) => {
+      if (!autoscroll) {
+        return
+      }
+
+      mutations
+        .find(
+          (mutation) =>
+            mutation.type === 'childList' &&
+            mutation.addedNodes.length === 1 &&
+            mutation.addedNodes[0] instanceof Element &&
+            mutation.addedNodes[0].matches(selector)
+        )
+        ?.addedNodes[0].scrollIntoView({
+          behavior: 'smooth'
+        })
+    },
+    [selector, autoscroll]
+  )
+
+  useEffect(() => {
+    if (!scrollContainerRef.current) {
+      return
+    }
+
+    const mutationObserver = new MutationObserver(mutationCallback)
+    mutationObserver.observe(scrollContainerRef.current, {
+      childList: true
+    })
+
+    return () => mutationObserver.disconnect()
+  }, [scrollContainerRef.current, mutationCallback])
+
+  return scrollContainerRef
+}
+
+const useToggleAutoscroll = (
+  autoscroll: boolean
+): UIEventHandler<HTMLOListElement> => {
+  const dispatch = useContext(DispatchContext)
+
+  return useCallback<UIEventHandler<HTMLOListElement>>(
+    ({ currentTarget }) => {
+      const isScrolledToBottom =
+        currentTarget.scrollHeight ===
+        // not exactly sure why, but sometime's there's an extra half-pixel
+        Math.floor(currentTarget.scrollTop + currentTarget.clientHeight + 1)
+
+      if (isScrolledToBottom && !autoscroll) {
+        dispatch(ActivateAutoscrollAction())
+      } else if (!isScrolledToBottom && autoscroll) {
+        dispatch(DeactivateAutoscrollAction())
+      }
+    },
+    [autoscroll, dispatch]
+  )
+}
+
+export const MessageList: FC<MessageListProps> = ({ messages, autoscroll }) => {
+  const scrollContainerRef = useAutoscrollTo(
+    '.message-list > :last-child',
+    autoscroll
+  )
+  const toggleAutoscroll = useToggleAutoscroll(autoscroll)
+
+  return (
+    <ol
+      className="message-list"
+      ref={scrollContainerRef}
+      onScroll={toggleAutoscroll}
+    >
+      {messages.map((message, i) => (
+        <li key={message.id}>
+          {message.type === MessageType.MovedRoom && <hr />}
+          <MessageView
+            message={message}
+            id={message.id}
+            hideTimestamp={shouldHideTimestamp(message, messages[i - 1])}
+            msgIndex={i}
+          />
+        </li>
+      ))}
+    </ol>
+  )
+}
