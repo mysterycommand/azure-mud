3d6f40a3ebc815dff5fe794e426eba54ee84e271
Matt Hayes <matt@mysterycommand.com>
2022-09-25 13:57:59 -0500

    feat: pull in updated ChatView from previous work

--- src/components/ChatView.tsx
+++ src/components/ChatView.tsx
@@ -1,63 +1,89 @@
-import React, { useContext } from 'react'
 import { findLastIndex } from 'lodash'
+import React, { useContext, useEffect, useState } from 'react'
 
-import MessageView from './MessageView'
-import { Message, MessageType, ConnectedMessage, DisconnectedMessage, EnteredMessage, LeftMessage } from '../message'
+import { isMovementMessage, Message, MessageType } from '../message'
 
-import '../../style/chat.css'
-import { DispatchContext } from '../App'
-import { ActivateAutoscrollAction, DeactivateAutoscrollAction } from '../Actions'
 import { ServerSettings } from '../../server/src/types'
+import '../../style/chat.css'
+import { SendMessageAction } from '../Actions'
+import { DispatchContext } from '../App'
+import { MessageList } from './MessageList'
 
-function isMovementMessage (message: Message): message is ConnectedMessage | DisconnectedMessage | EnteredMessage | LeftMessage {
-  return message.type === MessageType.Connected || message.type === MessageType.Disconnected ||
-    message.type === MessageType.Entered || message.type === MessageType.Left
-}
+/**
+ * necessary because TypeScript doesn't know we're in a browser context
+ *
+ * @see node_modules/typescript/lib/lib.dom.d.ts
+ * @see node_modules/@types/node/globals.d.ts
+ */
+declare function setTimeout(
+  handler: TimerHandler,
+  timeout?: number,
+  ...arguments: any[]
+): number;
+declare function clearTimeout(id: number | undefined): void;
 
 interface Props {
-  messages: Message[],
-  autoscrollChat: boolean,
-  serverSettings: ServerSettings,
-  captionsEnabled: boolean
+  messages: Message[];
+  autoscrollChat: boolean;
+  serverSettings: ServerSettings;
+  captionsEnabled: boolean;
+}
+
+/**
+ * explicitly adding `stopDebugMessageDispatcher` to the window so I can stop
+ * this thing after a few messages if I want to inspect stuff
+ */
+declare global {
+  interface Window {
+    stopDebugMessageDispatcher: () => void;
+  }
+}
+
+let timeoutId = 0
+if (process.env.NODE_ENV === 'development') {
+  window.stopDebugMessageDispatcher = () => clearTimeout(timeoutId)
+}
+
+const useDebugMessageDispatcher = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return
+  }
+
+  const dispatch = useContext(DispatchContext)
+
+  useEffect(() => {
+    const dispatchMessage = () =>
+      dispatch(SendMessageAction(`The time is now ${new Date()}.`))
+
+    // let timeoutId = 0
+    const scheduleMessage = () => {
+      timeoutId = setTimeout(
+        () => {
+          dispatchMessage()
+          scheduleMessage()
+        },
+        // sends a message every 5 + 0..10 seconds
+        (5 + Math.round(Math.random() * 10)) * 1_000
+      )
+    }
+
+    scheduleMessage()
+    return () => clearTimeout(timeoutId)
+  }, [])
 }
 
 export default function ChatView (props: Props) {
-  const dispatch = useContext(DispatchContext)
+  useDebugMessageDispatcher()
 
-  const handleScroll = () => {
-    const messageWindow = document.querySelector('#messages')
-    const isScrolledToBottom = messageWindow.scrollHeight === messageWindow.scrollTop + messageWindow.clientHeight
-
-    if (isScrolledToBottom && !props.autoscrollChat) {
-      dispatch(ActivateAutoscrollAction())
-    } else if (!isScrolledToBottom && props.autoscrollChat) {
-      dispatch(DeactivateAutoscrollAction())
-    }
-  }
-
-  React.useEffect(() => {
-    const lastMessage = document.querySelector(
-      '#messages .message-wrapper:last-of-type'
-    )
-    if (lastMessage && props.autoscrollChat) {
-      // I was using lastMessage.scrollIntoView()
-      // But I was seeing odd behavior when there was only one message on-screen.
-      // This very TS-unfriendly code fixes taht.
-      (lastMessage.parentNode as Element).scrollTop =
-        (lastMessage as any).offsetTop -
-        (lastMessage.parentNode as any).offsetTop
-    }
-  })
-
-  const [shouldShowOlderMessages, setShouldShowOlderMessages] = React.useState(false)
+  const [shouldShowOlderMessages, setShouldShowOlderMessages] = useState(false)
 
   // This message filtering logic is kinda ugly and hard to read
   function shouldRemoveMessage (m: Message) {
-    return isMovementMessage(m) &&
-      (
-        props.serverSettings.movementMessagesHideRoomIds.includes(m.roomId) ||
-        m.numUsersInRoom > props.serverSettings.movementMessagesHideThreshold
-      )
+    return (
+      isMovementMessage(m) &&
+      (props.serverSettings.movementMessagesHideRoomIds.includes(m.roomId) ||
+        m.numUsersInRoom > props.serverSettings.movementMessagesHideThreshold)
+    )
   }
   const messages = props.messages
     .filter((msg) => {
@@ -72,10 +98,12 @@ export default function ChatView (props: Props) {
 
   const lastIndexOfMovedMessage = findLastIndex(
     messages,
-    message => message.type === MessageType.MovedRoom
+    (message) => message.type === MessageType.MovedRoom
   )
   const currentRoomMessages = messages.slice(lastIndexOfMovedMessage)
-  const shownMessages = shouldShowOlderMessages ? messages : currentRoomMessages
+  const shownMessages = shouldShowOlderMessages
+    ? messages
+    : currentRoomMessages
 
   return (
     <>
@@ -85,43 +113,11 @@ export default function ChatView (props: Props) {
       >
         {shouldShowOlderMessages ? 'Hide' : 'Show'} Older Messages
       </button>
-      <div id="messages" onScroll={handleScroll}>
-        {shownMessages.slice(-150).map((m, idx) => {
-          let hideTimestamp = false
-          const previousMessage = props.messages[idx - 1]
-          if (previousMessage) {
-            // TODO: Give all messages a userId for this to be meaningful
-            if (
-              (previousMessage as any).userId &&
-          (m as any).userId &&
-          (previousMessage as any).userId === (m as any).userId
-            ) {
-              const diff =
-            new Date(m.timestamp).getTime() -
-            new Date(previousMessage.timestamp).getTime()
-              // This is a bad way to calculate '3 minutes' and I should feel bad -em
-              if (diff < 1000 * 60 * 3) {
-                hideTimestamp = true
-              }
-            }
-          }
-
-          const shouldShowInterstitial = m.type === MessageType.MovedRoom
-          const id = `message-${idx}`
-
-          return (
-            <>
-              {shouldShowInterstitial ? <hr key={id + '-interstitial'}/> : null}
-              <MessageView
-                message={m}
-                key={id}
-                id={id}
-                hideTimestamp={hideTimestamp}
-                msgIndex={idx}
-              />
-            </>
-          )
-        })}
+      <div id="messages">
+        <MessageList
+          messages={shownMessages}
+          autoscroll={props.autoscrollChat}
+        />
       </div>
     </>
   )
